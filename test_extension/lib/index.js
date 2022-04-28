48883843525f8d2dc3f7d74a5303e3802769e13c
gurhar1133 <ghns1133@gmail.com>
2022-04-28 19:49:20 +0000

    Fix #4071: slack link in launcher

--- test_extension/lib/index.js
+++ test_extension/lib/index.js
@@ -1,86 +1,40 @@
-
-// import {
-//   JupyterFrontEnd,
-//   JupyterFrontEndPlugin,
-// } from '@jupyterlab/application';
-
-// import { ICommandPalette } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
+import { LabIcon } from '@jupyterlab/ui-components';
+import slackIconStr from '../../sirepo/package_data/static/svg/slack.svg';
 
 export default [
   {
     id: 'test_extension',
     autoStart: true,
-    // requires: [ICommandPalette],
-    activate: function (app, palette) {
-      console.log(
-        'Menu Extension'
-      );
+    requires: [ILauncher],
+    activate: function (app, launcher) {
+
       console.log(app.commands);
       const { commands } = app;
-      const command = 'jlab-examples:main-menu';
+
+      const command = 'jlab-examples:slack-launcher';
+      const icon = new LabIcon({
+        name: 'launcher:slack-icon',
+        svgstr: slackIconStr,
+      });
+
       commands.addCommand(command, {
-        label: 'Open Sirepo Slack for Help',
-        caption: 'Execute jlab-examples:main-menu Command',
+        label: 'Sirepo Slack',
+        caption: 'Execute jlab-examples:slack-launcher Command',
+        icon,
         execute: (args) => {
           console.log(
-            `jlab-examples:main-menu has been called ${args['origin']}.`
+            `jlab-examples:slack-launcher has been called ${args['origin']}.`
           );
           window.location.assign("http://www.slack.com");
         },
       });
 
-      // Add the command to the command palette
-      // const category = 'Extension Examples';
-      // palette.addItem({
-      //   command,
-      //   category,
-      //   args: { origin: 'from the palette' },
-      // });
+      launcher.add({
+        command,
+        category: 'Other',
+        rank: 1,
+      });
     },
-    }
+  }
 ];
-
-
-// import {
-//   JupyterFrontEnd,
-//   JupyterFrontEndPlugin,
-// } from '@jupyterlab/application';
-
-// import { ICommandPalette } from '@jupyterlab/apputils';
-
-// /**
-//  * Initialization data for the main menu example.
-//  */
-// const extension: JupyterFrontEndPlugin<void> = {
-//   id: 'main-menu',
-//   autoStart: true,
-//   requires: [ICommandPalette],
-//   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-//     const { commands } = app;
-
-//     // Add a command
-//     const command = 'jlab-examples:main-menu';
-//     commands.addCommand(command, {
-//       label: 'Execute jlab-examples:main-menu Command',
-//       caption: 'Execute jlab-examples:main-menu Command',
-//       execute: (args: any) => {
-//         console.log(
-//           `jlab-examples:main-menu has been called ${args['origin']}.`
-//         );
-//         window.alert(
-//           `jlab-examples:main-menu has been called ${args['origin']}.`
-//         );
-//       },
-//     });
-
-//     // Add the command to the command palette
-//     const category = 'Extension Examples';
-//     palette.addItem({
-//       command,
-//       category,
-//       args: { origin: 'from the palette' },
-//     });
-//   },
-// };
-
-// export default extension;
