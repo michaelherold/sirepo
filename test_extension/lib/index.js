0102924f5472d024fb48bf964c0814fe5c9793b6
gurhar1133 <ghns1133@gmail.com>
2022-04-28 22:12:29 +0000

    Fix #4071: left sidebar wip

--- test_extension/lib/index.js
+++ test_extension/lib/index.js
@@ -1,6 +1,22 @@
 import { ILauncher } from '@jupyterlab/launcher';
 import { LabIcon } from '@jupyterlab/ui-components';
+import { Widget } from '@lumino/widgets';
 import slackIconStr from '../../sirepo/package_data/static/svg/slack.svg';
+
+const icon = new LabIcon({
+  name: 'launcher:slack-icon',
+  svgstr: slackIconStr,
+  id: 'icon-id'
+});
+
+class ExampleWidget extends Widget {
+  constructor() {
+    super();
+    this.addClass('slack-icon');
+    this.id = 'simple-widget-example';
+    this.title.icon = icon;
+  }
+}
 
 export default [
   {
@@ -9,32 +25,44 @@ export default [
     requires: [ILauncher],
     activate: function (app, launcher) {
 
-      console.log(app.commands);
-      const { commands } = app;
+
+      const { commands, shell } = app;
 
       const command = 'jlab-examples:slack-launcher';
-      const icon = new LabIcon({
-        name: 'launcher:slack-icon',
-        svgstr: slackIconStr,
-      });
+
 
       commands.addCommand(command, {
         label: 'Sirepo Slack',
         caption: 'Execute jlab-examples:slack-launcher Command',
         icon,
         execute: (args) => {
-          console.log(
-            `jlab-examples:slack-launcher has been called ${args['origin']}.`
-          );
-          window.location.assign("http://www.slack.com");
+          console.log('app.shell', app.shell);
+          // window.location.assign("http://www.slack.com");
         },
       });
+
+
+
+
 
       launcher.add({
         command,
         category: 'Other',
         rank: 1,
       });
+
+
+
+      const widget = new ExampleWidget();
+      var n = document.createElement('div');
+      n.innerHTML = '<div id="slack-widget" onclick="console.log(2)"> hello </div> ';
+      widget.node = n;
+      widget.id = 'simple-widget-example';
+      console.log('widget.node: ', widget.node);
+      shell.add(widget, 'left');
+
+
+
     },
   }
 ];
