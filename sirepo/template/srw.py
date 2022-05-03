bd61a66e648cb6d5b7471f8ac1e9aefc1e66232d
gurhar1133 <ghns1133@gmail.com>
2022-05-03 17:53:09 +0000

    Fix #4151: notes

--- sirepo/template/srw.py
+++ sirepo/template/srw.py
@@ -360,7 +360,15 @@ def extract_report_data(sim_in):
         out.units[1] = '[m]'
     else:
         out.units[1] = '({})'.format(out.units[1])
+    # TODO (gurhar1133): if r.contains watchpointanimation, then use h5py
+    # to get data, range else below method
+    # NOTE: See rsfbpic/rsdata/read_field_hdf.py for example of how to
+    # open and parse h5.
+    # NOTE: Switching default back to ascii and inspecting printed result
+    # of data and allrange will help with getting the formatting
     data, _, allrange, _, _ = uti_plot_com.file_load(out.filename)
+    pkdp(data)
+    pkdp(allrange)
     res = PKDict(
         title=out.title,
         subtitle=out.get('subtitle', ''),
