9bb2ecbc86be26c63ec8d9459165541048102b43
garsuga <gsugarbaker@gmail.com>
2022-10-05 20:06:48 +0000

    ckp

--- etc/run-auth-email.sh
+++ etc/run-auth-email.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
+set -eou pipefail
+
+if [[ ! -d ~/mail ]]; then
+    install -m 700 -d ~/mail
+    install -m 600 /dev/stdin ~/.procmailrc <<'END'
+UMASK=077
+:0
+mail/.
+END
+    sudo su - <<'END'
+        dnf install -y postfix procmail
+        postconf -e \
+            'mydestination=$myhostname, localhost.$mydomain, localhost, localhost.localdomain' \
+            mailbox_command=/usr/bin/procmail
+        systemctl enable postfix
+        systemctl restart postfix
+END
+    echo 'Testing mail delivery'
+    echo hello | sendmail vagrant@localhost.localdomain
+    sleep 4
+    if ! grep -s hello ~/mail/1; then
+        echo mail delivery test failed
+        exit 1
+    fi
+    rm ~/mail/1
+fi
+
 # see run-jupyterhub.sh for setting up local mail delivery
-export SIREPO_FROM_EMAIL='support@radiasoft.net'
+export SIREPO_FROM_EMAIL='$USER+support@localhost.localdomain'
 export SIREPO_FROM_NAME='RadiaSoft Support'
-export SIREPO_SMTP_PASSWORD='vagrant'
-# POSIT: same as sirepo.smtp.DEV_SMTP_SERVER
-export SIREPO_SMTP_SERVER='dev'
-export SIREPO_SMTP_USER='vagrant'
+export SIREPO_SMTP_SERVER='localhost'
+export SIREPO_SMTP_SEND_DIRECTLY=1
 export SIREPO_AUTH_METHODS='email:guest'
 export PYKERN_PKDEBUG_WANT_PID_TIME=1
 exec sirepo service http
